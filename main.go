082692f0734f1716b48da87bc8b16c00bdcd022e
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-11 19:51:10 +0200

    Print comments for Prometheus

--- main.go
+++ main.go
@@ -9,18 +9,17 @@ import (
 func main() {
 	endpoint := "/metrics"
 	http.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
-		CPUUsage, err := getCPUUsage()
+		cpu, err := getCPUUsage()
 		if err != nil {
 			log.Println("Failed to get CPU usage")
 			return
 		}
-		RAMUsage, err := getRAMUsage()
+		ram, err := getRAMUsage()
 		if err != nil {
 			log.Println("Failed to get RAM usage")
 			return
 		}
-		fmt.Fprintf(w, "cpu_usage_percent %.2f\n", CPUUsage)
-		fmt.Fprintf(w, "ram_usage_percent %.2f\n", RAMUsage)
+		writeMetrics(w, cpu, ram)
 	})
 
 	port := 8080
