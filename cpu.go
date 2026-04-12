17ffaa946236638b162be3082acd9bbd5697e94b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-12 11:01:32 +0200

    Run the program on the machine, not Docker

--- cpu.go
+++ cpu.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"time"
+
 	"github.com/shirou/gopsutil/v3/cpu"
 )
 
 func getCPUUsage() (float64, error) {
-	percent, err := cpu.Percent(0, false)
+	percent, err := cpu.Percent(time.Second, false)
 	if err != nil {
 		return 0.0, err
 	}
