de1e48349914628691113b25c0b29508ebab7721
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-13 22:03:15 +0200

    Add loop with sleep time of 5 seconds

--- svcmonitor/main.go
+++ svcmonitor/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello from svcmonitor")
-	resp, err := http.Get("http://localhost:3000")
-	if err != nil {
-		fmt.Println("Dead")
-		return
+	for {
+		resp, err := http.Get("http://localhost:3000")
+		if err != nil {
+			fmt.Println("Server listening at 3000 is down")
+		}
+		if resp.StatusCode != 200 {
+			fmt.Println("Server listening at 3000 is down")
+		} else {
+			fmt.Println("Server listening at 3000 is up")
+		}
+		time.Sleep(5 * time.Second)
 	}
-	fmt.Println("Alive", resp)
 }
