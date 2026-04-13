1fbe372742517ddf841e898b5d073ee0171085a3
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-13 19:14:37 +0200

    Test http.Get()

--- svcmonitor/main.go
+++ svcmonitor/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello from svcmonitor")
+	resp, err := http.Get("http://localhost:3000")
+	if err != nil {
+		fmt.Println("Dead")
+		return
+	}
+	fmt.Println("Alive", resp)
 }
