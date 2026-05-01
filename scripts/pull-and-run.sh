18d642e97dbd4ee59aca0b83e6efb38b4a61341a
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-01 12:20:47 +0200

    Add change directory to script

--- scripts/pull-and-run.sh
+++ scripts/pull-and-run.sh
@@ -3,6 +3,8 @@
 LOCAL=$(git log --oneline | head -1)
 REMOTE=$(git log --oneline origin/main | head -1)
 
+cd ~/hauslab
+
 if [[ $LOCAL != $REMOTE ]]; then
     git pull
     cd ../services && make up
