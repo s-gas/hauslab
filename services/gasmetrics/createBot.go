acb29315dbc7fca9ee33f75f24e18f9248df0f9b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-10 12:00:53 +0200

    Replace err with fmt.Errorf()

--- services/gasmetrics/createBot.go
+++ services/gasmetrics/createBot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -9,11 +10,11 @@ import (
 func createBot() (*tgbotapi.BotAPI, error) {
 	token, err := os.ReadFile("/run/secrets/telegram_token")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("createBot: %w", err)
 	}
 	bot, err := tgbotapi.NewBotAPI(string(token))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("createBot: %w", err)
 	}
 	return bot, nil
 }
--- services/gasmetrics/generateUrl.go
+++ services/gasmetrics/generateUrl.go
@@ -9,7 +9,7 @@ func generateUrl() (string, error) {
 	user := os.Getenv("POSTGRES_USER")
 	password, err := os.ReadFile("/run/secrets/postgres_password")
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("generateUrl: %w\n", err)
 	}
 	db := os.Getenv("POSTGRES_DB")
 	url := fmt.Sprintf("postgres://%s:%s@postgres:5432/%s", user, password, db)
