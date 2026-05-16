e291fefcfec32f8fa62e9273918d49a7d2e77428
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-16 19:59:14 +0200

    Fix variable name

--- services/gasmetrics/gasmetrics-server/internal/handler/validation.go
+++ services/gasmetrics/gasmetrics-server/internal/handler/validation.go
@@ -8,5 +8,5 @@ func validate(value int, lastEntry int) (int, error) {
 	if value <= lastEntry {
 		return 0, fmt.Errorf("validate: value must be greater than last entry (%v)", lastEntry)
 	}
-	return v, nil
+	return value, nil
 }
