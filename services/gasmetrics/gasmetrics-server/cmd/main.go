123f628b4e13e383737448a6f81c43b4112aa621
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-16 18:54:35 +0200

    Add logs for every error

--- services/gasmetrics/gasmetrics-server/cmd/main.go
+++ services/gasmetrics/gasmetrics-server/cmd/main.go
@@ -27,15 +27,17 @@ func main() {
 	endpoint := "/readings"
 	http.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
+			log.Println("method not allowed")
 			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 		if err := json.NewDecoder(r.Body).Decode(&reading); err != nil {
+			log.Println("invalid body")
 			http.Error(w, "invalid body", http.StatusBadRequest)
 			return
 		}
-		log.Println("request received with value", reading.value)
 		if err := handler.StoreValue(ctx, conn, reading.value); err != nil {
+			log.Println(err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
