ad30f23c7a32f2b219e28508da18a771b7d129d9
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-19 18:23:04 +0200

    Remove cobra comments

--- services/gasmetrics/gasmetrics-cli/cmd/add.go
+++ services/gasmetrics/gasmetrics-cli/cmd/add.go
@@ -17,8 +17,6 @@ var addCmd = &cobra.Command{
 	Short: 	"Add a gas reading (int)",
 	Args:		cobra.ExactArgs(1),
 	Run:		func(cmd *cobra.Command, args []string) {
-		url := "http://hauslab/readings"
-		contentType := "application/json"
 		body := strings.NewReader(fmt.Sprintf(`{"value": %s}`, args[0]))
 		resp, err := http.Post(url, contentType, body)
 		if err != nil {
@@ -36,14 +34,4 @@ var addCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- services/gasmetrics/gasmetrics-cli/cmd/root.go
+++ services/gasmetrics/gasmetrics-cli/cmd/root.go
@@ -6,19 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	url = "http://hauslab/readings"
+	contentType = "application/json"
+)
 
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gasmetrics-cli",
 	Short: "CLI tool to submit gas readings",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -27,14 +25,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gasmetrics-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
